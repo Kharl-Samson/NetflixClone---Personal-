359d4c373e84402032934c1c42dc535601692c09
Kharl Samson <khrlsmsn1110@gmail.com>
2023-02-10 14:19:48 +0800

    test

--- src/Components/TrendingNow.jsx
+++ src/Components/TrendingNow.jsx
@@ -136,30 +136,38 @@ export default function TrendingNow() {
       document.getElementById("progress_bar_trending").style.display = "none"
       document.getElementById("my_modal_trending").style.display = "block"
       loadTrailer_Trending();
-      playVideo_Trending()
+      if(loaded === true){
+        playVideo_Trending()
+      }
     }, 700);
   }
   function close_info(){
     for (var x = 0 ; x < document.getElementsByClassName("list_container").length ; x++){
       document.getElementsByClassName("list_container")[x].style.zIndex = "1"
     }
-    stopVideo_Trending()
+    if(loaded === true){
+      stopVideo_Trending()
+    }
     settrailerId_trending(null);
+    setLoaded(false)
     document.getElementById("youtube_modal_trending").style.display = "none"
     document.getElementById("progress_bar_trending").style.display = "block"
     document.getElementById("my_modal_trending").style.display = "none"
     document.getElementById("mediaType_key_Trending").value = null
   }
 
+  const [loaded, setLoaded] = useState(false);
   const loadTrailer_Trending = async () => {
     const res = await axios.get(`${API_BASE_URL}/${document.getElementById("mediaType_key_Trending").value}/${MOVIE_ID_TRENDING}/videos?api_key=${API_KEY}`);
     for(var i = 0 ; i < res.data.results.length ; i++){
       if (res.data.results[i].name.toUpperCase().indexOf('OFFICIAL TRAILER') > -1)
       {
+        setLoaded(true)
         settrailerId_trending(res.data.results[i].key);
         break;
       }
       else{
+        setLoaded(false)
         settrailerId_trending(null);
       }
     }
