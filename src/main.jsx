19d9bae8271c3b350382f36507e3ab691a6f2f1e
Kharl Samson <khrlsmsn1110@gmail.com>
2023-02-10 13:07:16 +0800

    test

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,3 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registration successful');
-      })
-      .catch(err => {
-        console.error('Service Worker registration failed:', err);
-      });
-  });
-}
